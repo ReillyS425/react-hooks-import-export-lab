ec62440966988d54028b42be16889b10eb14ded8
Reilly Shofner <reilly.shofner@gmail.com>
2025-04-30 15:10:22 -0700

    complete react-hooks-import-export-lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-function Home() {
+import { username, city } from "../data/user.js"
+ function Home() {
   return (
     <div id="home">
       <h1>
@@ -9,3 +9,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
